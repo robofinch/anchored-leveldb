bf0ea962f64327718f0097a25564a92c124085cc
robofinch <86991566+robofinch@users.noreply.github.com>
2025-10-04 21:56:03 -0400

    More TableOptions conversions

--- crates/anchored-sstable/src/option_structs.rs
+++ crates/anchored-sstable/src/option_structs.rs
@@ -13,6 +13,29 @@ pub struct ReadTableOptions<CompList, Policy, TableCmp, Cache, Pool> {
     pub buffer_pool:      Pool,
 }
 
+impl<CompList, Policy, TableCmp, Cache, Pool, S> MirroredClone<S>
+for ReadTableOptions<CompList, Policy, TableCmp, Cache, Pool>
+where
+    CompList: MirroredClone<S>,
+    Policy:   MirroredClone<S>,
+    TableCmp: MirroredClone<S>,
+    Cache:    MirroredClone<S>,
+    Pool:     MirroredClone<S>,
+    S:        Speed,
+{
+    #[inline]
+    fn mirrored_clone(&self) -> Self {
+        Self {
+            compressor_list:  self.compressor_list.mirrored_clone(),
+            filter_policy:    self.filter_policy.mirrored_clone(),
+            comparator:       self.comparator.mirrored_clone(),
+            verify_checksums: self.verify_checksums,
+            block_cache:      self.block_cache.mirrored_clone(),
+            buffer_pool:      self.buffer_pool.mirrored_clone(),
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct WriteTableOptions<CompList, Policy, TableCmp> {
     pub compressor_list:        CompList,
@@ -34,6 +57,30 @@ pub struct WriteTableOptions<CompList, Policy, TableCmp> {
     ///
     /// [`Block`]: super::block::Block
     pub block_size:             usize,
+    /// Whether to sync the table file to persistent storage once the file is complete.
+    pub sync_table:             bool,
+}
+
+impl<CompList, Policy, TableCmp, S> MirroredClone<S>
+for WriteTableOptions<CompList, Policy, TableCmp>
+where
+    CompList: MirroredClone<S>,
+    Policy:   MirroredClone<S>,
+    TableCmp: MirroredClone<S>,
+    S:        Speed,
+{
+    #[inline]
+    fn mirrored_clone(&self) -> Self {
+        Self {
+            compressor_list:        self.compressor_list.mirrored_clone(),
+            selected_compressor:    self.selected_compressor,
+            filter_policy:          self.filter_policy.mirrored_clone(),
+            comparator:             self.comparator.mirrored_clone(),
+            block_restart_interval: self.block_restart_interval,
+            block_size:             self.block_size,
+            sync_table:             self.sync_table,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -42,6 +89,9 @@ pub struct TableOptions<CompList, Policy, TableCmp, Cache, Pool> {
     pub selected_compressor:    u8,
     pub filter_policy:          Option<Policy>,
     pub comparator:             TableCmp,
+    pub verify_checksums:       bool,
+    pub block_cache:            Cache,
+    pub buffer_pool:            Pool,
     /// The [`Block`]s of the table will have exactly one `restart` entry every
     /// `block_restart_interval` entries. These restart entries are used by iterators seeking
     /// through the `Block`, including moving backwards. (Forwards step-by-step iteration does not
@@ -57,54 +107,45 @@ pub struct TableOptions<CompList, Policy, TableCmp, Cache, Pool> {
     ///
     /// [`Block`]: super::block::Block
     pub block_size:             usize,
-    pub verify_checksums:       bool,
-    pub block_cache:            Cache,
-    pub buffer_pool:            Pool,
+    /// Whether to sync the table file to persistent storage once the file is complete.
+    pub sync_table:             bool,
 }
 
 impl<CompList, Policy, TableCmp, Cache, Pool>
     TableOptions<CompList, Policy, TableCmp, Cache, Pool>
 {
+    #[expect(clippy::type_complexity, reason = "long generic lists, but flat structure")]
     #[inline]
     #[must_use]
-    pub fn mirrored_read_opts<S>(
-        &self,
-    ) -> ReadTableOptions<CompList, Policy, TableCmp, Cache, Pool>
-    where
-        CompList: MirroredClone<S>,
-        Policy:   MirroredClone<S>,
-        TableCmp: MirroredClone<S>,
-        Cache:    MirroredClone<S>,
-        Pool:     MirroredClone<S>,
-        S:        Speed,
-    {
-        ReadTableOptions {
-            compressor_list:  self.compressor_list.mirrored_clone(),
-            filter_policy:    self.filter_policy.mirrored_clone(),
-            comparator:       self.comparator.mirrored_clone(),
-            verify_checksums: self.verify_checksums,
-            block_cache:      self.block_cache.mirrored_clone(),
-            buffer_pool:      self.buffer_pool.mirrored_clone(),
-        }
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn mirrored_write_opts<S>(&self) -> WriteTableOptions<CompList, Policy, TableCmp>
+    pub fn split<S>(self) -> (
+        ReadTableOptions<CompList, Policy, TableCmp, Cache, Pool>,
+        WriteTableOptions<CompList, Policy, TableCmp>,
+    )
     where
         CompList: MirroredClone<S>,
         Policy:   MirroredClone<S>,
         TableCmp: MirroredClone<S>,
         S:        Speed,
     {
-        WriteTableOptions {
-            compressor_list:        self.compressor_list.mirrored_clone(),
-            selected_compressor:    self.selected_compressor,
-            filter_policy:          self.filter_policy.mirrored_clone(),
-            comparator:             self.comparator.mirrored_clone(),
-            block_restart_interval: self.block_restart_interval,
-            block_size:             self.block_size,
-        }
+        (
+            ReadTableOptions {
+                compressor_list:        self.compressor_list.mirrored_clone(),
+                filter_policy:          self.filter_policy.mirrored_clone(),
+                comparator:             self.comparator.mirrored_clone(),
+                verify_checksums:       self.verify_checksums,
+                block_cache:            self.block_cache,
+                buffer_pool:            self.buffer_pool,
+            },
+            WriteTableOptions {
+                compressor_list:        self.compressor_list,
+                selected_compressor:    self.selected_compressor,
+                filter_policy:          self.filter_policy,
+                comparator:             self.comparator,
+                block_restart_interval: self.block_restart_interval,
+                block_size:             self.block_size,
+                sync_table:             self.sync_table,
+            },
+        )
     }
 }
 
@@ -126,29 +167,6 @@ for ReadTableOptions<CompList, Policy, TableCmp, Cache, Pool>
 }
 
 impl<CompList, Policy, TableCmp, Cache, Pool>
-    From<&TableOptions<CompList, Policy, TableCmp, Cache, Pool>>
-for ReadTableOptions<CompList, Policy, TableCmp, Cache, Pool>
-where
-    CompList: Clone,
-    Policy:   Clone,
-    TableCmp: Clone,
-    Cache:    Clone,
-    Pool:     Clone,
-{
-    #[inline]
-    fn from(opts: &TableOptions<CompList, Policy, TableCmp, Cache, Pool>) -> Self {
-        Self {
-            compressor_list:  opts.compressor_list.clone(),
-            filter_policy:    opts.filter_policy.clone(),
-            comparator:       opts.comparator.clone(),
-            verify_checksums: opts.verify_checksums,
-            block_cache:      opts.block_cache.clone(),
-            buffer_pool:      opts.buffer_pool.clone(),
-        }
-    }
-}
-
-impl<CompList, Policy, TableCmp, Cache, Pool>
     From<TableOptions<CompList, Policy, TableCmp, Cache, Pool>>
 for WriteTableOptions<CompList, Policy, TableCmp>
 {
@@ -161,27 +179,7 @@ for WriteTableOptions<CompList, Policy, TableCmp>
             comparator:             opts.comparator,
             block_restart_interval: opts.block_restart_interval,
             block_size:             opts.block_size,
-        }
-    }
-}
-
-impl<CompList, Policy, TableCmp, Cache, Pool>
-    From<&TableOptions<CompList, Policy, TableCmp, Cache, Pool>>
-for WriteTableOptions<CompList, Policy, TableCmp>
-where
-    CompList: Clone,
-    Policy:   Clone,
-    TableCmp: Clone,
-{
-    #[inline]
-    fn from(opts: &TableOptions<CompList, Policy, TableCmp, Cache, Pool>) -> Self {
-        Self {
-            compressor_list:        opts.compressor_list.clone(),
-            selected_compressor:    opts.selected_compressor,
-            filter_policy:          opts.filter_policy.clone(),
-            comparator:             opts.comparator.clone(),
-            block_restart_interval: opts.block_restart_interval,
-            block_size:             opts.block_size,
+            sync_table:             opts.sync_table,
         }
     }
 }
