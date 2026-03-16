e8deb39b11c956d420d672b6e14d5cad211cf563
robofinch <86991566+robofinch@users.noreply.github.com>
2026-03-16 13:47:36 -0400

    Implement `TableFooter` and `MetaindexComparator`

---
+++ crates/anchored-leveldb/src/sstable/file_format.rs
@@ -0,0 +1,105 @@
+use std::cmp::Ordering;
+
+use crate::{
+    all_errors::types::TableFooterCorruption,
+    pub_traits::cmp_and_policy::FilterPolicy,
+    pub_typed_bytes::BlockHandle,
+};
+
+
+/// The length of the footer at the end of every (compressed) block of an SSTable.
+pub(crate) const BLOCK_FOOTER_LEN: usize = const {
+    let footer_len: usize = 5;
+
+    assert!(
+        footer_len == size_of::<u8>() + size_of::<u32>(),
+        "Show why it's the magic value 5",
+    );
+
+    footer_len
+};
+
+/// The hardcoded `filter.` prefix used before a filter's name (in metaindex block entries
+/// corresponding to filters).
+pub(super) const FILTER_META_PREFIX: &[u8] = b"filter.";
+
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) struct TableFooter {
+    pub metaindex: BlockHandle,
+    pub index:     BlockHandle,
+}
+
+#[expect(unreachable_pub, reason = "control visibility at type definition")]
+impl TableFooter {
+    /// The exact length of the table footer (when encoded).
+    const ENCODED_LENGTH:    usize   = 48;
+    const MAGIC:             u64     = 0x_db47_7524_8b80_fb57;
+    pub const ENCODED_MAGIC: [u8; 8] = Self::MAGIC.to_le_bytes();
+
+    pub fn decode_from(input: &[u8; Self::ENCODED_LENGTH]) -> Result<Self, TableFooterCorruption> {
+        #[expect(clippy::unwrap_used, reason = "8 <= 48")]
+        let magic: &[u8; 8] = input.last_chunk().unwrap();
+        if magic != &Self::ENCODED_MAGIC {
+            return Err(TableFooterCorruption::BadTableMagic(*magic));
+        }
+
+        let (metaindex, metaindex_size) = BlockHandle::decode(input)
+            .map_err(|err| TableFooterCorruption::BlockHandle(0, err))?;
+
+        // Encoded block handles consist of two varint64's, each of which can be up to 10 bytes
+        // long.
+        #[expect(
+            clippy::as_conversions,
+            clippy::cast_possible_truncation,
+            reason = "overflow/wrap impossible; `metaindex_size <= 20 < u8::MAX`",
+        )]
+        let metaindex_size_u8 = metaindex_size as u8;
+
+        #[expect(clippy::indexing_slicing, reason = "`metaindex_size + 20 <= 40 < input.len()`")]
+        let (index, _) = BlockHandle::decode(&input[metaindex_size..])
+            .map_err(|err| TableFooterCorruption::BlockHandle(metaindex_size_u8, err))?;
+
+        Ok(Self {
+            metaindex,
+            index,
+        })
+    }
+
+    pub fn encode_to(&self, output: &mut [u8; Self::ENCODED_LENGTH]) {
+        #[expect(clippy::unwrap_used, reason = "`20 < Self::ENCODED_LENGTH == 48`")]
+        let metaindex_len = self.metaindex.encode(output.first_chunk_mut::<20>().unwrap());
+
+        #[expect(clippy::indexing_slicing, reason = "`metaindex_len <= 20 < 48`")]
+        let remaining = &mut output[metaindex_len..];
+        #[expect(
+            clippy::unwrap_used,
+            reason = "`metaindex_len + 20 <= 40 < 48`",
+        )]
+        let _ignore_index_len = self.index.encode(remaining.first_chunk_mut::<20>().unwrap());
+
+        #[expect(clippy::unwrap_used, reason = "`8 < 48`")]
+        {
+            *output.last_chunk_mut::<8>().unwrap() = Self::ENCODED_MAGIC;
+        };
+    }
+}
+
+/// Regardless of the comparator settings of a LevelDB database, its metaindex blocks always use
+/// the normal lexicographic ordering on byte slices.
+#[derive(Debug, Clone, Copy)]
+pub(super) struct MetaindexComparator;
+
+#[expect(unreachable_pub, reason = "control visibility at type definition")]
+impl MetaindexComparator {
+    /// Compare the given `key` against the name of the given `Policy` (with an added
+    /// [`FILTER_META_PREFIX`]), with respect to the ordering used by the metaindex block of an
+    /// SSTable (namely, the lexicographic ordering on byte slices).
+    pub fn key_cmp_policy_name<Policy: FilterPolicy>(key: &[u8], policy: &Policy) -> Ordering {
+        if let Some(key_filter_name) = key.strip_prefix(FILTER_META_PREFIX) {
+            key_filter_name.cmp(policy.name())
+        } else {
+            key.cmp(FILTER_META_PREFIX)
+        }
+    }
+}
