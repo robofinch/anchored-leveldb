8c1226a0e216ba579bab3c18bc7985056403f4fa
robofinch <86991566+robofinch@users.noreply.github.com>
2026-04-17 13:31:06 -0400

    Remove old contention_queue module

--- crates/anchored-leveldb/src/contention_queue/mod.rs
+++
@@ -1,8 +0,0 @@
-mod queue;
-mod task;
-mod ad_hoc_variance_family_trait;
-
-pub(crate) use self::{
-    ad_hoc_variance_family_trait::{AdHocCovariantFamily, VaryingWriteCommand, WriteCommand},
-    queue::{ContentionQueue, PanicOptions, ProcessResult, ProcessTask, QueueHandle},
-};
